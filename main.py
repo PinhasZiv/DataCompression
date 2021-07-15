f5a40718857602b2a90558adfbbced95e49f026a
Pinhas <piniziv8@gmail.com>
2021-07-15 19:54:09 +0300

    dealing with binary files separate the file_str into list of str in size block_size generating freq_table based on the current string

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from ArithmeticCoding.ae import Aencoder
 
-table = {'a': 1, 'b': 3}
 
 path = "C:\\Users\פינחס זיו\PycharmProjects\DataCompression\\venv\ArithmeticCoding\\newFile.txt"
 
-x1 = Aencoder(path, table, 4)
+x1 = Aencoder(path, 100)
 
 x1.get_file_txt()
 print(x1.get_size())
-x1.get_prob_table()
 x1.generate_str_list()
+x1.get_prob_table()
+x1.get_interval_table()
+
 x1.print1()
-x1.encode_str(x1.file_str)
+x1.encode_file()
--- venv/ArithmeticCoding/ae.py
+++ venv/ArithmeticCoding/ae.py
@@ -3,19 +3,20 @@
 class Aencoder:
     size = 0
     file_str = None
+    freq_table = {}
     prob_table = {}
+    interval_table = {}
     str_list = []
     tag_list = []
 
 
-    def __init__(self, path, freq_table, block_size):
+    def __init__(self, path, block_size):
         self.path = path
-        self.freq_table = freq_table
         self.block_size = block_size
 
     # get the text from the file into a String field 'file_str'. initial block_size
     def get_file_txt(self):
-        with open(self.path) as file:
+        with open(self.path, 'rb') as file:
             self.file_str = file.read()
         print(self.file_str)
         self.size = len(self.file_str)
@@ -24,11 +25,25 @@ class Aencoder:
 
     # convert freq_table into probability table
     def get_prob_table(self):
+        for str in self.str_list:
+            for symbol in str:
+                if symbol in self.freq_table:
+                    self.freq_table[symbol] += 1
+                else:
+                    self.freq_table[symbol] = 1
         total = sum(list(self.freq_table.values()))
         sum1 = 0
         for key, value in self.freq_table.items():
-            self.prob_table[key] = [value/total, sum1]
-            sum1 += self.prob_table[key][0]
+            self.prob_table[key] = value/total
+
+
+    def get_interval_table(self):
+        sum1 = 0
+        for key, value in self.prob_table.items():
+            current_prob = value
+            self.interval_table[key] = (sum1, sum1+current_prob)
+            sum1 += current_prob
+
 
     # create a list of Strings based on block_size (to make the commpression easier)
     def generate_str_list(self):
@@ -54,53 +69,44 @@ class Aencoder:
             i -= 1
         return str
 
-    def scaling(self, left, right, ctr):
-        if (left >= 0.0) & (right < 0.5):
-            left *= 2
-            right *= 2
+    def scaling(self, low, high, ctr, output):
+        if (low >= 0.0) and (high < 0.5):
+            low *= 2
+            high *= 2
             output += '0'
             output += self.add_c(ctr, '1')
             ctr = 0
-            return self.scaling(left, right, ctr)
-        elif (left >= 0.5) & (right < 1.0):
-            left = 2 * left - 1
-            right = 2 * right - 1
+            return self.scaling(low, high, ctr, output)
+        elif (low >= 0.5) and (high < 1.0):
+            low = 2 * low - 1
+            high = 2 * high - 1
             output += '1'
             output += self.add_c(ctr, '0')
             ctr = 0
-            return self.scaling(left, right, ctr)
-        elif (left >= 0.25) & (right < 0.75):
-            left = 2 * left - 0.5
-            right = 2 * right - 0.5
+            return self.scaling(low, high, ctr, output)
+        elif (low >= 0.25) and (high < 0.75):
+            low = 2 * low - 0.5
+            high = 2 * high - 0.5
             ctr += 1
-            return self.scaling(left, right, ctr)
-        return left, right, ctr
+            return self.scaling(low, high, ctr, output)
+        return low, high, ctr, output
+
+    def encode_file(self):
+        for str in self.str_list:
+            self.tag_list = self.encode_str(str)
+
+
 
     def encode_str(self, str):
-        left, right, ctr = 0, 1, 0
+        low, high, ctr = 0, 1, 0
         output = ""
         for char in str:
-            w = right-left
-            left = left + w*self.prob_table[char][1]
-            right = left + w*self.prob_table[char][0]
-            left, right, ctr = self.scaling(left, right, ctr)
-            # if (left >= 0.0) & (right < 0.5):
-            #     left *= 2
-            #     right *= 2
-            #     output += '0'
-            #     output += self.add_c(ctr, '1')
-            #     ctr = 0
-            # elif (left >= 0.5) & (right < 1.0):
-            #     left = 2*left - 1
-            #     right = 2*right - 1
-            #     output += '1'
-            #     output += self.add_c(ctr, '0')
-            #     ctr = 0
-            # elif (left >= 0.25) & (right < 0.75):
-            #     left = 2*left - 0.5
-            #     right = 2*right - 0.5
-            #     ctr += 1
-        tag = (left+right) / 2
-        print(tag)
-        print(output)
+            range = high-low
+            high = low + self.interval_table[char][1]*range
+            low = low + self.interval_table[char][0]*range
+
+            low, high, ctr, output = self.scaling(low, high, ctr, output)
+        tag = (low+high) / 2
+        print("output: ", output)
+        return tag
 
