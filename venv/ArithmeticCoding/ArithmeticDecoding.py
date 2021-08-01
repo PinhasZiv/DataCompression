72ce29ed4dfa31ef7d8b47f9b54c8a6568af7c6c
Pinhas <piniziv8@gmail.com>
2021-08-01 11:15:24 +0300

    hexa implementation version

--- venv/ArithmeticCoding/ArithmeticDecoding.py
+++ venv/ArithmeticCoding/ArithmeticDecoding.py
@@ -1,25 +1,30 @@
 import math
 from decimal import Decimal, getcontext
+import os
 
 getcontext().prec = 20
 
+
 class Adecoder:
 
-    interval_table = {}
-    change_interval_table = {}
-
-    def __init__(self, path, num, prob, size_str):
+    def __init__(self, path, prob, size_str, num=0):
         self.path = path
         self.num = num
         self.prob_table = prob
         self.size_str = size_str
-
-
+        self.interval_table = {}
+        self.change_interval_table = {}
+        self.file_str = ""
 
     # get the text from the file into a String field 'file_str'.
     def get_file_txt(self):
         with open(self.path, 'rb') as file:
-            self.file_str = file.read()
+            file_size = os.stat(self.path).st_size
+            for i in range(file_size):
+                num = int.from_bytes(file.read(1), "big")
+                num2 = num % 16
+                num1 = num // 16
+                self.file_str += hex(num1)[2] + hex(num2)[2]
         # print(self.file_str)
         self.size = len(self.file_str)
 
@@ -33,76 +38,74 @@ class Adecoder:
 
     def fill_num(self, str_num, digit):
         size = len(str_num)
-        while size < 16:
+        while size < 18:
             str_num = str_num + digit
             size = size + 1
         return str_num
 
     def decode_num(self):
-        low, high, ctr = '0000000000000000', '9999999999999999', 0
+        low, high, ctr = '0x' + '0' * 16, '0x' + 'f' * 16, 0
         rest = 0
-        if len(self.num) <= 16:
+        self.num = "0x" + self.file_str
+        if len(self.num) <= 18:
             num = self.fill_num(self.num, '0')
         else:
-            num = self.num[:16]
-            rest = self.num[16:]
+            num = self.num[:18]
+            rest = self.num[18:]
         output = ""
-        hight = int(high) - int(low) + 1
+        height = int(high, 16) - int(low, 16) + 1
         for i in range(self.size_str):
             for symbol, value in self.interval_table.items():
-                # print("symbol:", symbol, ", high_value:", str(math.floor(int(low) + value[1]*hight - 1)), ", low_value:", str(math.floor(int(low) + value[0] * hight)),
+                # print("symbol:", symbol, ", high_value:", str(math.floor(int(low) + value[1]*height - 1)), ", low_value:", str(math.floor(int(low) + value[0] * height)),
                 #       ", num:", num)
 
-
-                if int(num) >= int(math.floor(int(low) + value[0] * hight)) and int(num) < int(math.floor(int(low) + value[1] * hight - 1)):
-                #if int(num) >= int(math.floor(int(low) + value[0] * hight)) and int(str(int(num))[:15] +'0') < int(str(int(math.floor(int(low) + value[1] * hight - 1)))[:15]+'1'):
+                if int(math.floor(int(low, 16) + value[0] * height)) <= int(num, 16) < int(
+                        math.floor(int(low, 16) + value[1] * height - 1)):
+                    # if int(num) >= int(math.floor(int(low) + value[0] * height)) and int(str(int(num))[:15] +'0') < int(str(int(math.floor(int(low) + value[1] * height - 1)))[:15]+'1'):
 
                     output = output + chr(symbol)
-                    high = str(math.floor(int(low) + value[1] * hight - 1))
-                    low = str(math.floor(int(low) + value[0] * hight))
+                    high = hex(math.floor(int(low, 16) + value[1] * height - 1))
+                    low = hex(math.floor(int(low, 16) + value[0] * height))
 
                     # need to find how should we know which type needed to convert back?
-                    # print("symbol:", symbol, "high:", high, ", low:", low, ", hight:", hight, ", num:", num)
+                    # print("symbol:", symbol, "high:", high, ", low:", low, ", height:", height, ", num:", num)
 
-                    while len(low) < 16:
-                        low = '0' + low
-                    while len(high) < 16:
-                        high = '0' + high
+                    while len(low) < 18:
+                        low = low[:2] + '0' + low[2:]
+                    while len(high) < 18:
+                        high = high[:2] + '0' + high[2:]
 
-                    while high[0] == low[0]:
-                        num = num[1:]
-                        high = high[1:]
-                        low = low[1:]
-                        high = high + '9'
+                    while high[2] == low[2]:
+                        num = '0x' + num[3:]
+                        high = '0x' + high[3:]
+                        low = '0x' + low[3:]
+                        high = high + 'f'
                         low = low + '0'
                         if rest != 0 and len(rest) > 0:
-                            num = num + rest[0]
+                            num = '0x' + num[2:] + rest[0]
                             rest = rest[1:]
                         else:
-                            num = num + '0'
+                            num = '0x' + num[2:] + '0'
 
-
-                    # need to check how to recover this if stetment
-                    while int(high[0]) - int(low[0]) == 1 and (low[1] == '9' and high[1] == '0'):
+                    while int(high[2], 16) - int(low[2], 16) == 1 and (low[3] == 'f' and high[3] == '0'):
                         # if low[1] == '9' and high[1] == '0':
                         #     print("before scaling:", "high:", high, ", low:", low, )
-                            low = low[0] + low[2:] + '0'
-                            high = high[0] + high[2:] + '9'
-                            num = num[0] + num[2:]
-                            if rest != 0 and len(rest) > 0:
-                                num = num + rest[0]
-                                rest = rest[1:]
-                            else:
-                                num = num + '0'
-                            # print("after scaling:", "high:", high, ", low:", low, )
+                        low = '0x' + low[2] + low[4:] + '0'
+                        high = '0x' + high[2] + high[4:] + 'f'
+                        num = '0x' + num[2] + num[4:]
+                        if rest != 0 and len(rest) > 0:
+                            num = '0x' + num[2:] + rest[0]
+                            rest = rest[1:]
+                        else:
+                            num = '0x' + num[2:] + '0'
+                        # print("after scaling:", "high:", high, ", low:", low, )
 
-                    hight = int(high) - int(low) + 1
+                    height = int(high, 16) - int(low, 16) + 1
                     break
 
-
-
         print("decompressed file:", output)
+        path = r"C:\Users\פינחס זיו\Desktop\venv\ArithmeticCoding\longFileOutDecompressed.bmp"
+        out = open(path, "w", newline='')
+        out.write(str(output))
         print(len(output))
-
-
-
+        out.close()
